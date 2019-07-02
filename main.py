767143c6b7870b5332410f6f22ac2d7e5e12119f
AlexCovizzi <alexcovizzi@gmail.com>
2019-07-02 11:56:10 +0200

    aggiunta un'altra greedy, aggiunta first improvement 2 swap
    Co-authored-by: AlessioMorselli <ale.mors.22@gmail.com>

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def create_graph(n, threshold = 50):
             return graph
 
 
-def print_graph(graph, f="graph.txt", removed=[]):
+def print_graph(graph, removed=[], f="graph.txt"):
     with open(f, "w") as f:
         for i in range(len(graph)):
             if i in removed:
@@ -34,21 +34,15 @@ def print_graph(graph, f="graph.txt", removed=[]):
                     f.write(str(i) + " " + str(j) + "\n")
 
 
-def algo_greedy(graph, k):
-    graph1 = [row[:] for row in graph]
+def algo_greedy(graph, k, best):
     removed = []
     for i in range(k):
-        removed.append(algo_greedy_iter(graph1, removed))
-    return graph1, removed
+        best_vertex = best(graph, removed)
+        removed.append(best_vertex)
+    return removed
 
 
-def algo_greedy_iter(graph, removed):
-    best_vertex = first_best(graph, removed)
-    # remove_vertex(graph, best_vertex)
-    return best_vertex
-
-
-def first_best(graph, removed):
+def max_degree_best(graph, removed):
     deg_vertices = [sum([n for idx,n in enumerate(row) if idx not in removed]) for row in graph]
     for i in removed:
         deg_vertices[i] = -1
@@ -56,11 +50,26 @@ def first_best(graph, removed):
     max_vertex = deg_vertices.index(max_deg)
     return max_vertex
 
-
-def remove_vertex(graph, vertex):
-    graph[vertex] = [0 for i in graph]
-    for row in graph:
-        row[vertex] = 0
+# buono con tanti nodi e grafo meno sparso
+def min_conn_best(graph, removed):
+    cur_best_couple = ()
+    cur_best_val = 10000000
+    deg_vertices = [sum([n for idx,n in enumerate(row) if idx not in removed]) for row in graph]
+    for node in range(len(graph)):
+        if node in removed or deg_vertices[node] == 0:
+            continue
+        for node_2, conn in enumerate(graph[node]):
+            if conn == 1 and node_2 not in removed and node_2 > node:
+                couple_degree = deg_vertices[node] + deg_vertices[node_2]
+                if couple_degree < cur_best_val:
+                    cur_best_couple = (node, node_2)
+                    cur_best_val = couple_degree
+    
+    # restituisco il nodo con grado maggiore nella coppia
+    if deg_vertices[cur_best_couple[0]] > deg_vertices[cur_best_couple[1]]:
+        return cur_best_couple[0]
+    else:
+        return cur_best_couple[1]
 
 
 def visit_connected(graph, i, visited):
@@ -105,7 +114,7 @@ def neighbour_search(graph, removed, mossa):
         if sol > best:
             best = sol
         else:
-            shaking 
+            shaking
 
 def k_swap(graph, removed, k=1):
     k = k if k < len(removed) else len(removed)
@@ -121,6 +130,7 @@ def k_swap(graph, removed, k=1):
         removed.append(swapped_left)
         left.append(swapped_removed)
 
+# TODO: implementare tabu search con best 1 swap
 def best_1_swap(graph, removed):
     left = [node for node in range(len(graph)) if node not in removed]
     sol = calc_objective(graph, removed)
@@ -138,22 +148,87 @@ def best_1_swap(graph, removed):
         r, l = best_swap
         removed[removed.index(r)] = l
 
-if __name__ == '__main__':
-    graph = create_graph(30, threshold=90)
-    print_graph(graph, "graph_1.txt")
-    graph_2, removed = algo_greedy(graph, 7)
-    print_graph(graph_2, "graph_2.txt", removed)
 
-    best = calc_objective(graph, removed)
-    print(best)
-    print(removed)
+def first_improvement_2_swap(graph, removed):
+    left = [node for node in range(len(graph)) if node not in removed]
+    sol = calc_objective(graph, removed)
+    best_swap = None
+    for r1 in removed:
+        for r2 in [r for r in removed if r != r1]:
+            for l1 in left:
+                for l2 in [l for l in left if l != l1]:
+                    removed_copy = removed[:]
+                    removed_copy[removed_copy.index(r1)] = l1
+                    removed_copy[removed_copy.index(r2)] = l2
+                    sol_swapped = calc_objective(graph, removed_copy)
+                    if sol_swapped > sol:
+                        removed[removed.index(r1)] = l1
+                        removed[removed.index(r2)] = l2
+                        return True
+
+
+if __name__ == '__main__':
+    graph = create_graph(40, threshold=80)
+    k = 20
+    print_graph(graph)
+    removed_1 = algo_greedy(graph, k, max_degree_best)
+    print_graph(graph, removed_1, "graph_1.txt")
+    removed_2 = algo_greedy(graph, k, min_conn_best)
+    best_1 = calc_objective(graph, removed_1)
+    best_2 = calc_objective(graph, removed_2)
+
+    print("max_degree")
+    print(best_1)
+    print(removed_1)
+
+    print("min_conn")
+    print(best_2)
+    print(removed_2)
+
+    best_11 = best_1
+    removed_11 = removed_1[:]
+    
+    i1 = 0
     while True:
-        best_1_swap(graph, removed)
-        sol = calc_objective(graph, removed)
-        if sol > best:
-            best = sol
+        i1 += 1
+        best_1_swap(graph, removed_1)
+        sol_1 = calc_objective(graph, removed_1)
+        if sol_1 > best_1:
+            best_1 = sol_1
         else:
             break
     
-    print(removed)
-    print(best)
+    i2 = 0
+    while True:
+        i2 += 1
+        best_1_swap(graph, removed_11)
+        sol_11 = calc_objective(graph, removed_11)
+        if sol_11 >= best_11 and i2 < 1000:
+            best_11 = sol_11
+        else:
+            break
+
+    while True:
+        best_1_swap(graph, removed_2)
+        sol_2 = calc_objective(graph, removed_2)
+        if sol_2 > best_2:
+            best_2 = sol_2
+        else:
+            break
+    
+    print("------------------------------")
+
+    print("max_degree - 1-swap")
+    print("Numero iterazioni: " + str(i1))
+    print(best_1)
+    print(removed_1)
+
+    print("max_degree - 2-swap")
+    print("Numero iterazioni: " + str(i2))
+    print(best_11)
+    print(removed_11)
+
+    print("min_conn - 1-swap")
+    print(best_2)
+    print(removed_2)
+    
